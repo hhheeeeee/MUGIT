d4a20c950553d69d964ba4d1ce44929bea148646
남상엽 <spearoad15@gmail.com>
2024-04-15 18:00:45 +0900

    init springboot

---
+++ src/test/java/com/ssafy/mugit/MugitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.mugit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MugitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
