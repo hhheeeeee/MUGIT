af8906502c49c8d77bb18a96f749d4eb37d37703
이준학 <0710june@naver.com>
2024-04-23 11:14:34 +0900

    feat: 파일서버 시작

---
+++ src/main/java/com/ssafy/mugit/MugitApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.mugit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MugitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MugitApplication.class, args);
+	}
+
+}
