43e934d5c2152217e0726e1dd90b110111f6a27e
김승희 <libravickyyy@gmail.com>
2024-04-25 17:44:57 +0900

    feat. footer layout 추가

--- mugit/src/app/page.tsx
+++ mugit/src/app/page.tsx
@@ -1,24 +1,28 @@
 import Line from "./container/landing/line";
+import Footer from "./components/footer";
 
 export default function Home() {
   return (
-    <main
-      className="flex h-[90%] w-full flex-auto flex-col content-center items-center justify-center
+    <>
+      <main
+        className="flex h-[90%] w-full flex-auto flex-col content-center items-center justify-center
       bg-pointblack"
-    >
-      s
-      <p
-        className="flex w-full flex-col items-center justify-between bg-pointblack text-[14rem]
-          font-extrabold italic text-pointyellow"
       >
-        MUGIT
-      </p>
-      <div className="w-full items-center justify-between bg-pointblack">
-        <Line />
-        <p className="mt-5 text-center text-[2rem] font-bold text-pointyellow">
-          Make your own Flow
+        s
+        <p
+          className="flex w-full flex-col items-center justify-between bg-pointblack text-[14rem]
+          font-extrabold italic text-pointyellow"
+        >
+          MUGIT
         </p>
-      </div>
-    </main>
+        <div className="w-full items-center justify-between bg-pointblack">
+          <Line />
+          <p className="mt-5 text-center text-[2rem] font-bold text-pointyellow">
+            Make your own Flow
+          </p>
+        </div>
+      </main>
+      <Footer />
+    </>
   );
 }
