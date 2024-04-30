04169cd11127700d982fc679a6404e6e23a3268f
김승희 <libravickyyy@gmail.com>
2024-04-30 10:16:10 +0900

    fix. Bottomplaybar layout 수정

--- mugit/src/app/components/bottomPlaybar/BottomPlaybar.tsx
+++ mugit/src/app/components/bottomPlaybar/BottomPlaybar.tsx
@@ -21,13 +21,13 @@ const BottomPlaybar = () => {
       {playing === true ? (
         <>
           <div className="z-40 pt-4"></div>
-          <div className="fixed bottom-0 left-0 flex h-24 w-full items-center justify-center bg-pointblue px-10 ">
+          <div className="fixed bottom-0 left-0 flex h-24 w-full items-center bg-pointblue px-10 ">
             <div className="relative h-20 w-20 rounded-lg bg-red-500"></div>
             <div className="mx-4 flex flex-col">
-              <p className="mb-2 text-2xl font-bold text-pointyellow">
+              <span className="mb-2 text-2xl font-bold text-pointyellow">
                 {song?.title}
-              </p>
-              <p className="text-pointyellow">{song?.name}</p>
+              </span>
+              <span className="text-pointyellow">{song?.name}</span>
             </div>
             <BottomController />
             <div
