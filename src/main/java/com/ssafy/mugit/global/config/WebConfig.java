79453fdc6b0406ca2877e2dbd11a7a0b4ce5eb04
이준학 <0710june@naver.com>
2024-05-02 16:01:00 +0900

    fix: 파일 저장 경로 수정

--- src/main/java/com/ssafy/mugit/global/config/WebConfig.java
+++ src/main/java/com/ssafy/mugit/global/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/files/iamge/**")
-                .addResourceLocations("file:///file");
+        registry.addResourceHandler("/files/**")
+                .addResourceLocations("file:///file/");
     }
 }
