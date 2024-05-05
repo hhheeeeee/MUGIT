d1ed28fef1130f7ba3d53a35206c7a04607b2bd3
SangyeopNam <spearoad15@gmail.com>
2024-05-05 22:02:21 +0900

    test : 기존 테스트 정상 성공

--- src/main/java/com/ssafy/mugit/infrastructure/RedisConfig.java
+++ src/main/java/com/ssafy/mugit/infrastructure/RedisConfig.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -22,11 +23,18 @@ public class RedisConfig {
     @Value("${spring.data.redis.host}")
     private String host;
 
+    @Value("${spring.data.redis.password}")
+    private String password;
+
     private final ObjectMapper objectMapper;
 
     @Bean
     public LettuceConnectionFactory connectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(host, port);
+        redisStandaloneConfiguration.setPassword(password);
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
+        lettuceConnectionFactory.setEagerInitialization(true);
+        return lettuceConnectionFactory;
     }
 
     @Bean
--- src/test/java/com/ssafy/mugit/domain/message/api/MessageApiControllerTest.java
+++ src/test/java/com/ssafy/mugit/domain/message/api/MessageApiControllerTest.java
@@ -5,8 +5,7 @@ import com.ssafy.mugit.domain.message.fixture.MessageDtoFixture;
 import jakarta.servlet.http.Cookie;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -24,11 +23,13 @@ import static com.ssafy.mugit.domain.message.fixture.MessageDtoFixture.MESSAGE_D
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class MessageApiControllerTest {
 
     @Autowired
@@ -39,13 +40,14 @@ class MessageApiControllerTest {
 
     @Test
     @DisplayName("api 전송 테스트 : 로그인 안되어있을때(404)")
+    @Order(1)
     void testApi404() throws Exception {
         // given
         long userId = 1L;
         String content = objectMapper.writeValueAsString(MESSAGE_DTO_01.getFixture());
 
         // when
-        ResultActions perform = mvc.perform(get("/message")
+        ResultActions perform = mvc.perform(post("/message")
                 .contentType(APPLICATION_JSON)
                 .content(content));
 
@@ -57,6 +59,7 @@ class MessageApiControllerTest {
 
     @Test
     @DisplayName("api 전송 테스트")
+    @Order(2)
     void testApi200() throws Exception {
         // given
         Cookie[] cookies = mvc.perform(get("/mock/login")).andReturn().getResponse().getCookies();
@@ -64,7 +67,7 @@ class MessageApiControllerTest {
         String content = objectMapper.writeValueAsString(MESSAGE_DTO_01.getFixture());
 
         // when
-        ResultActions perform = mvc.perform(get("/message")
+        ResultActions perform = mvc.perform(post("/message")
                 .contentType("application/json")
                 .content(content));
 
