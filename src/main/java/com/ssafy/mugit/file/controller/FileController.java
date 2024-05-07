45157b83f27a5921a9556028a7f79be41eac6d0e
이준학 <0710june@naver.com>
2024-05-07 12:55:05 +0900

    fix: cors 수정

--- src/main/java/com/ssafy/mugit/file/controller/FileController.java
+++ src/main/java/com/ssafy/mugit/file/controller/FileController.java
@@ -16,7 +16,6 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/files")
-@CrossOrigin("*")
 @Slf4j
 public class FileController {
 
--- src/main/java/com/ssafy/mugit/global/config/WebConfig.java
+++ src/main/java/com/ssafy/mugit/global/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.ssafy.mugit.global.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,5 +16,14 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/files/**")
                 .addResourceLocations("file://" + uploadPath);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로에 대해
+                .allowedOrigins("https://mugit.site", "http://localhost") // 이 출발지에서 오는 요청 허용
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메소드
+                .allowedHeaders("*") // 모든 헤더 허용
+                .allowCredentials(true); // 쿠키를 포함한 요청 허용
     }
 }
