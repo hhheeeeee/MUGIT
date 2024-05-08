e118d9302a6d660af523b1bb3574fbcb3f96d6ec
김승희 <libravickyyy@gmail.com>
2024-05-08 16:53:41 +0900

    fix. next.config 설정에서 note 제거

--- mugit/next.config.mjs
+++ mugit/next.config.mjs
@@ -29,11 +29,11 @@ const nextConfig = {
         destination: "https://mugit.site/api/users/profiles", // 배포된 백엔드 서버의 URL로 변경하세요.
         locale: false,
       },
-      {
-        source: "/note",
-        destination: "https://mugit.site/api/flows/note", // 배포된 백엔드 서버의 URL로 변경하세요.
-        locale: false,
-      },
+      //   {
+      //     source: "/note",
+      //     destination: "https://mugit.site/api/flows/note", // 배포된 백엔드 서버의 URL로 변경하세요.
+      //     locale: false,
+      //   },
     ];
   },
   trailingSlash: true,
