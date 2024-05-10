c4649db8535c287c9a265d2cf2e458b63c815561
이준학 <0710june@naver.com>
2024-05-10 10:28:13 +0900

    feat: 소스 테이블 정리 스케쥴러 구현

---
+++ src/main/java/com/ssafy/mugit/global/config/SchedulerConfig.java
@@ -0,0 +1,23 @@
+package com.ssafy.mugit.global.config;
+
+import com.ssafy.mugit.record.service.SourceCronService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+public class SchedulerConfig {
+
+    private final SourceCronService sourceCronService;
+
+    @Scheduled(cron = "0 0 * * * *")
+    public void sourceJob(){
+        log.info("[[[ 소스 테이블 작업 스케쥴링 시작 ]]]");
+        sourceCronService.cleanSource();
+        log.info("[[[ 소스 테이블 작업 스케쥴링 종료 ]]]");
+    }
+
+}
