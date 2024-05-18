e53efb1b02db2d0e67b44ad3858c9436dc6e7293
hhheeeeee <libravickyyy@gmail.com>
2024-05-18 14:22:35 +0900

    feat. google login loading 중 화면 수정

--- mugit/src/app/[locale]/sns-login/page.tsx
+++ mugit/src/app/[locale]/sns-login/page.tsx
@@ -8,45 +8,46 @@ import { userAtom } from "@/app/store/atoms/user";
 import Cookies from "js-cookie";
 import { prevpathAtom } from "@/app/store/atoms/user";
 import fireToast from "@/app/utils/fireToast";
+import Loading from "@/app/components/loading";
 
 export default function Page() {
   const locale = useLocale();
   const setUser = useSetAtom(userAtom);
   const prevpath = useAtomValue(prevpathAtom);
 
-  useEffect(() => {
-    const accessToken = window.location.hash.split("=")[1].split("&")[0];
-    fetch(apiUrl + "/users/login", {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-      credentials: "include",
-    })
-      .then((response) => {
-        switch (response.status) {
-          case 200: {
-            setUser({
-              id: String(Cookies.get("userId")),
-              isLogined: String(Cookies.get("isLogined")),
-              nickName: String(Cookies.get("nickName")),
-              profileImagePath: String(Cookies.get("profileImagePath")),
-              profileText: String(Cookies.get("profileText")),
-              followerCount: String(Cookies.get("followers")),
-              followingCount: String(Cookies.get("followings")),
-            });
+  // useEffect(() => {
+  //   const accessToken = window.location.hash.split("=")[1].split("&")[0];
+  //   fetch(apiUrl + "/users/login", {
+  //     headers: {
+  //       Authorization: `Bearer ${accessToken}`,
+  //     },
+  //     credentials: "include",
+  //   })
+  //     .then((response) => {
+  //       switch (response.status) {
+  //         case 200: {
+  //           setUser({
+  //             id: String(Cookies.get("userId")),
+  //             isLogined: String(Cookies.get("isLogined")),
+  //             nickName: String(Cookies.get("nickName")),
+  //             profileImagePath: String(Cookies.get("profileImagePath")),
+  //             profileText: String(Cookies.get("profileText")),
+  //             followerCount: String(Cookies.get("followers")),
+  //             followingCount: String(Cookies.get("followings")),
+  //           });
 
-            if (prevpath) {
-              location.href = prevpath;
-            }
-            break;
-          }
-          case 302: {
-            location.href = `/${locale}/register`;
-            break;
-          }
-        }
-      })
-      .catch((error) => console.log(error));
-  });
-  return <div>로그인 중... 로딩중 화면 넣기</div>;
+  //           if (prevpath) {
+  //             location.href = prevpath;
+  //           }
+  //           break;
+  //         }
+  //         case 302: {
+  //           location.href = `/${locale}/register`;
+  //           break;
+  //         }
+  //       }
+  //     })
+  //     .catch((error) => console.log(error));
+  // });
+  return <Loading />;
 }
